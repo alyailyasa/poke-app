ff6d75494803a75d56dd7e8fdc58811ae1d3cecf
alyailyasa <84371051+alyailyasa@users.noreply.github.com>
2024-05-17 15:52:51 +0700

    up

---
+++ src/app/detail/[id]/page.js
@@ -0,0 +1,132 @@
+'use client'
+
+import { React, useEffect, useState } from 'react'
+import { useParams, useRouter } from 'next/navigation'
+import Link from 'next/link'
+
+const typeColors = {
+  fire: 'bg-orange-500',
+  grass: 'bg-green-500',
+  dark: 'bg-yellow-900',
+  water: 'bg-blue-500',
+  bug: 'bg-yellow-600',
+  flying: 'bg-red-500',
+  poison: 'bg-purple-600',
+  normal: 'bg-yellow-300',
+  fairy: 'bg-pink-400',
+  ground: 'bg-neutral-600',
+  electric: 'bg-red-400',
+  fighting: 'bg-red-800'
+} 
+
+const PokemonDetails = () => {
+  const [pokemon, setPokemon] = useState(null)
+  const { id } = useParams()
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!id) return
+
+    const fetchPokemonDetail = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_POKEMON_API_URL}/${id}`
+        )
+        if (response.ok) {
+          const data = await response.json()
+          setPokemon(data)
+        } else {
+          console.error('Error fetching Pokémon details:', response.statusText)
+        }
+      } catch (error) {
+        console.error('Error fetching Pokémon details:', error)
+      }
+    }
+
+    fetchPokemonDetail();
+  }, [id]);
+
+  if (!pokemon) {
+    return  <div className="flex flex-col gap-2 items-center justify-center h-screen">
+              <div className="animate-spin rounded-full h-32 w-32 border-t-4 border-b-4 border-purple-500"></div>
+              <p>Loading...</p>
+            </div>
+  }
+
+  const nextPokemonId = parseInt(id) + 1
+  const prevPokemonId = parseInt(id) - 1
+
+  const handleNavigation = (newId) => {
+    router.push(`/detail/${newId}`)
+  }
+
+  return (
+    <div className="container mx-auto p-4">
+      <Link href="/" className="flex flex-row items-center gap-1">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokewidth="1.5" stroke="currentColor" className="w-5 h-5 text-blue-500">
+          <path strokelinecap="round" strokelinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+        </svg>
+        <p  className="text-blue-500">Back to Home</p>
+      </Link>
+      <div className="flex justify-center items-center">
+        <div className="bg-gradient-to-r from-purple-200 via-purple-300 to-pink-100 w-[800px] h-[500px] p-5 rounded-xl flex flex-row items-center justify-center mt-8">
+          <div className="w-1/3 flex flex-col items-center">
+            <img src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} className="w-40 h-40" />
+          </div>
+          <div className="w-2/3 flex flex-col items-center justify-center">
+            <div className="bg-purple-400 w-full p-1 flex flex-row justify-between items-center">
+              <div className="flex gap-2 items-center justify-center">
+               <button onClick={() => handleNavigation(prevPokemonId)}>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokewidth="1.5" stroke="currentColor" className="w-5 h-5">
+                    <path strokelinecap="round" strokelinejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+                  </svg>
+                </button>
+                <img src={pokemon.sprites.other['official-artwork'].front_default} alt={pokemon.name} className="w-5 h-5" />
+                <p className="text-lg font-bold capitalize text-center">No. {pokemon.id}</p>
+              </div>
+              <p className="text-lg font-bold capitalize text-center">{pokemon.name}</p>
+              <button onClick={() => handleNavigation(nextPokemonId)}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                </svg>
+              </button>
+            </div>
+            <div className="mt-5 bg-purple-400 w-full p-1 flex flex-row justify-center items-center">
+              <p className="text-lg font-bold capitalize text-center">Pokemon Detail</p>
+            </div>
+            <div className="mt-1 bg-purple-400 w-full p-1 flex flex-row gap-5 justify-center items-center">
+              <div className="flex flex-col gap-1 text-md">
+                <p>Type</p>
+                <p>Height</p>
+                <p>Weight</p>
+                <p>Number Battled</p>
+              </div>
+              <div className="flex flex-col gap-1 text-md">
+                <div className="flex flex-row gap-2">
+                  {pokemon.types.map(({ type }) => (
+                    <p key={type.name} className={`text-white text-sm px-3 py-1 rounded-xl ${typeColors[type.name]}`}>
+                      {type.name}
+                    </p>
+                  ))}
+                </div>
+                <p>{pokemon.height}</p>
+                <p>{pokemon.weight}</p>
+                <p>{pokemon.base_experience}</p>
+              </div>
+            </div>
+            <div className="mt-1 bg-purple-400 w-full p-1 flex flex-col gap-2 justify-center items-center">
+              <h2 className="text-lg font-semibold mb-2">Abilities</h2>
+              <ul>
+                {pokemon.abilities.map(({ ability }) => (
+                  <li key={ability.name} className="capitalize text-md">•{ability.name}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PokemonDetails
---
+++ src/app/page.js
@@ -0,0 +1,101 @@
+'use client'
+
+import { React, useState, useEffect } from 'react'
+import Link from 'next/link'
+
+const typeColors = {
+  fire: 'bg-orange-500',
+  grass: 'bg-green-500',
+  dark: 'bg-yellow-900',
+  water: 'bg-blue-500',
+  bug: 'bg-yellow-600',
+  flying: 'bg-red-500',
+  poison: 'bg-purple-600',
+  normal: 'bg-yellow-300',
+  fairy: 'bg-pink-400',
+  ground: 'bg-neutral-600',
+  electric: 'bg-red-400',
+  fighting: 'bg-red-800'
+} 
+
+const HomePage = () => {
+  const [pokemons, setPokemons] = useState([]) 
+  const [searchInput, setSearchInput] = useState('')
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_POKEMON_API_URL}?limit=60&offset=0`)
+      const data = await res.json()
+
+      const detailedPokemons = await Promise.all(data.results.map(async (pokemon) => {
+        const res = await fetch(pokemon.url)
+        const details = await res.json()
+        return {
+          name: pokemon.name,
+          image: details.sprites.front_default,
+          id: details.id,
+          types: details.types.map(typeInfo => typeInfo.type.name),
+        }
+      })) 
+
+      setPokemons(detailedPokemons)
+    } 
+
+    fetchPokemons() 
+  }, []) 
+
+  const filteredPokemons = pokemons.filter(pokemon =>
+    pokemon.name.toLowerCase().includes(searchInput.toLowerCase())
+  )
+
+  return (
+    <div className="container mx-auto lg:p-[60px] md:p-[40px] sm:p-[30px] p-[20px]">
+      <div className="flex justify-end items-center mb-8">
+        <div className="relative">
+          <input 
+            className="rounded-lg px-3 py-2 w-[300px] border border-[#d67eff] pr-10" 
+            placeholder="Search pokemon"
+            value={searchInput}
+            onChange={(e)=>setSearchInput(e.target.value)}
+          />
+          <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            viewBox="0 0 24 24" 
+            strokeWidth="1.5" 
+            stroke="currentColor" 
+            className="w-6 h-6 absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500"
+          >
+            <path 
+              strokeLinecap="round" 
+              strokeLinejoin="round" 
+              d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" 
+            />
+          </svg>
+        </div>
+      </div>
+      <h1 className="text-4xl font-bold mb-8 text-center">Pokemon App</h1>
+      <div className="mt-24 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4">
+        {filteredPokemons.map((pokemon) => (
+          <Link key={pokemon.id} href={`/detail/${pokemon.id}`}>
+            <div className="border border-[#d67eff] shadow-md hover:shadow-lg transition flex flex-col">
+              <div className="flex justify-center items-center mb-4">
+                <img src={pokemon.image} alt={pokemon.name} className="w-20 h-20" />
+              </div>
+              <h3 className="text-lg font-semibold capitalize p-4">{pokemon.name}</h3>
+              <div className="bg-[#d67eff] flex flex-row gap-2 justify-center p-1">
+                {pokemon.types.map((type) => (
+                  <span key={type} className={`text-white text-sm px-3 py-0 rounded-xl ${typeColors[type]}`}>
+                    {type}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
